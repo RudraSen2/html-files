9e49bef2ae10cde261a973d2a63d83d1c11d0546
Comp Labs <complabs28@gmail.com>
2022-03-05 22:06:38 +0530

    Initial Commit

---
+++ urlshorten.php
@@ -0,0 +1,8 @@
+fetch('https://api-ssl.bitly.com/v4/shorten', {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Bearer {TOKEN}',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" })
+});
